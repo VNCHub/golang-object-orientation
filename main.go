f154d69a160987ac29c1486c53af9cd702d73220
vinicius <viniciuscarrocine@gmail.com>
2023-07-11 10:21:22 -0300

    Adicionando declaração de ponteiros

--- main.go
+++ main.go
@@ -10,7 +10,15 @@ type ContaCorrente struct {
 }
 
 func main() {
+	//Como declarar valores
 	contaVinicius := ContaCorrente{"Vinicius", 123, 001, 100}
 
-	fmt.Println(contaVinicius)
+	//Como declarar ponteiros
+	contaDenise := new(ContaCorrente)
+	contaDenise.titular = "Denise"
+	contaDenise.numeroAgencia = 123
+	contaDenise.numeroConta = 002
+	contaDenise.Saldo = 200
+
+	fmt.Println(contaVinicius, *contaDenise)
 }
