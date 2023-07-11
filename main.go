2ad64b227d746e3113b065b9f3c96710d44dbf73
vinicius <viniciuscarrocine@gmail.com>
2023-07-11 10:38:41 -0300

    Adicionado método de saque

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ type ContaCorrente struct {
 	titular       string
 	numeroAgencia int
 	numeroConta   int
-	Saldo         float64
+	saldo         float64
+}
+
+// sintaxe de um método = func + (objeto) + nome da função + (parametros) + retorno
+func (c *ContaCorrente) sacar(valor float64) bool {
+	podeSacar := valor <= c.saldo && valor > 0
+	fmt.Println("Realizando saque no valor de: R$", valor, "...")
+	if podeSacar {
+		c.saldo -= valor
+	}
+	return podeSacar
 }
 
 func main() {
@@ -18,7 +28,13 @@ func main() {
 	contaDenise.titular = "Denise"
 	contaDenise.numeroAgencia = 123
 	contaDenise.numeroConta = 002
-	contaDenise.Saldo = 200
+	contaDenise.saldo = 1000
+
+	if contaDenise.sacar(495.50) {
+		fmt.Println("Saque realizado com sucesso!")
+	} else {
+		fmt.Println("Não foi possível realizar o saque, saldo insuficiente.")
+	}
 
 	fmt.Println(contaVinicius, *contaDenise)
 }
