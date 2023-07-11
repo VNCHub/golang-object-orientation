4a355b02e338091192b4c8dc37fb81e6340b9628
vinicius <viniciuscarrocine@gmail.com>
2023-07-11 10:52:05 -0300

    Adicionado método de depósito

--- main.go
+++ main.go
@@ -19,6 +19,15 @@ func (c *ContaCorrente) sacar(valor float64) bool {
 	return podeSacar
 }
 
+func (c *ContaCorrente) depositar(valor float64) bool {
+	podeDepositar := valor > 0
+	fmt.Println("Realizando deposito no valor de: R$", valor, "...")
+	if podeDepositar {
+		c.saldo += valor
+	}
+	return podeDepositar
+}
+
 func main() {
 	//Como declarar valores
 	contaVinicius := ContaCorrente{"Vinicius", 123, 001, 100}
@@ -29,6 +38,12 @@ func main() {
 	contaDenise.numeroAgencia = 123
 	contaDenise.numeroConta = 002
 	contaDenise.saldo = 1000
+
+	if contaDenise.depositar(399.99) {
+		fmt.Println("Deposito realizado com sucesso!")
+	} else {
+		fmt.Println("Não foi possível realizar o depósito, valor negativo.")
+	}
 
 	if contaDenise.sacar(495.50) {
 		fmt.Println("Saque realizado com sucesso!")
