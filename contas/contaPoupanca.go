3a3e35d3a9c45e8496f40430ba93240162fd78f5
vinicius <viniciuscarrocine@gmail.com>
2023-07-11 22:27:01 -0300

    Corrigido erro com arquivo 'go.mod'

--- contas/contaPoupanca.go
+++ contas/contaPoupanca.go
@@ -1,13 +1,32 @@
 package contas
 
 import (
+	"fmt"
 	"orientacao-a-objetos/titulares"
 )
 
-type contaPoupanca struct {
+type ContaPoupanca struct {
 	Titular       titulares.PessoaFisica
 	NumeroAgencia int
 	NumeroConta   int
 	Operacao      int
 	saldo         float64
+}
+
+func (c *ContaPoupanca) Sacar(valor float64) bool {
+	podeSacar := valor <= c.saldo && valor > 0
+	fmt.Println("Realizando saque no valor de: R$", valor, "...")
+	if podeSacar {
+		c.saldo -= valor
+	}
+	return podeSacar
+}
+
+func (c *ContaPoupanca) Depositar(valor float64) bool {
+	podeDepositar := valor > 0
+	fmt.Println("Realizando deposito no valor de: R$", valor, "...")
+	if podeDepositar {
+		c.saldo += valor
+	}
+	return podeDepositar
 }
